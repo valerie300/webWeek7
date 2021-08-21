c67d26131e26bbe5baa7596572321541ffc42561
valerie300 <zxcv841219595@gmail.com>
2021-08-21 19:23:32 +0800

    Update 2021-08-21T11:23:26.043Z

--- app/assets/js/all.js
+++
@@ -1,5 +0,0 @@
-AOS.init({
-  duration: 1000, //秒數
-  once: false, //特效顯示一次 ture開 false關,會一直顯示
-  offset: 140, //什麼時候觸發
-});
---
+++ assets/js/all.js
@@ -0,0 +1,11 @@
+"use strict";
+
+AOS.init({
+  duration: 1000,
+  //秒數
+  once: false,
+  //特效顯示一次 ture開 false關,會一直顯示
+  offset: 140 //什麼時候觸發
+
+});
+//# sourceMappingURL=all.js.map
